d5f43f90e9ec306b67837d13a1319215f2999cba
IrinabanMaz <72575510+IrinabanMaz@users.noreply.github.com>
2021-05-26 13:01:24 -0400

    Initial Upload

---
+++ Legendre.h
@@ -0,0 +1,112 @@
+#include <vector>
+#include<cmath>
+
+#pragma once
+int factorial(int n)
+{
+    int ret = 1;
+    if (n < 0)
+    {
+        std::cout << "Error: negative integer passed to factorial";
+        return -1;
+    }
+    else for (int i = 2; i <= n; i++)
+        ret *= i;
+
+    return ret;
+}
+
+//Class which computes the value of the Associated Legendre Functions using recurrence relations. The class stores all intermediate values (over m,n)for a fixed input
+// for potential later use.
+class Legendre
+{
+
+
+private:
+    //the value of the input variable to the function.
+    double input = 100;
+    //the maximum value of the parameter m computed for this value of the input.
+    int maxorder = 0;
+    //the maximum value of the degree n computed for this value of the input.
+    int maxdegree = 0;
+    std::vector<std::vector<double>> values;
+    ;
+
+public:
+
+    Legendre()
+    {
+        input = 100;
+        maxorder = -1;
+        maxdegree = -1;
+    }
+
+    ///Populate the values of the legendre polynomials at the gicen point values.
+    ///-double val: The point we are evaluating at.
+    ///-int M: the maximum order of the legendre polynomials
+    ///int L: the maximum degree of the polynomials.
+    void populate(double val, int M, int L)
+    {
+
+
+        input = val;
+        maxorder = abs(M);
+        maxdegree = L;
+        double s = sqrt(1 - val * val) + 10e-12;
+
+        
+
+
+        values.resize(abs(M) + 1);
+
+        for (int m = 0; m < abs(M) + 1; m++)
+            values[m].resize(std::max(L + 1 , 2));
+        
+        //set values in case of boundary condition.
+        if (1.0 - val < 10e-6)
+            for (int i = 0; i < L; i++)
+                for (int m = 0; m < M; m++)
+                    values[m][i] = 1.0;
+
+        //set initial values.
+        values[0][0] = 1;
+        values[0][1] = input;
+
+        //set values for m = 0 using recurrence in terms of two previous i values.
+        for (int i = 1; i < L; i++)
+            values[0][i + 1] = ((2 * (double)i + 1) * input * values[0][i] - i * values[0][i - 1]) / ((double)i + 1);
+
+
+        //computes values for larger values of m in terms of previous value of m. for i = 0, uses the values from i = 1, i = 2, instead.
+        for (int m = 0; m < abs(M); m++)
+        {
+
+            for (int i = 1; i < L + 1; i++)
+                values[m + 1][i] = ((((double)i - (double)m)) * input * values[m][i] - ((double)i + (double)m) * values[m][i - 1]) / s;
+
+            if (maxdegree > 1)
+                values[m + 1][0] = (m * values[m + 1][2] + 3 * input * values[m + 1][1]) / ((double)m + 1);
+
+
+        }
+
+
+    }
+
+    //Accesses the value of the legendre functions for specified values of m,i.
+    double getValue(int m, int i)
+    {
+        if (m >= 0)
+            return values[m][i];
+        else if (m < 0)
+            return ((m % 2 == 0) ? 1 : -1) * (double)factorial(i - m) / factorial(i + m) * values[-m][i];
+
+        else return -100;
+    }
+
+};
+double testpoly(double x)
+{
+    double xsq = 1 - x * x;
+    return 105 * xsq * xsq;
+}
---
+++ SphericalCalc.h
@@ -0,0 +1,328 @@
+#pragma once
+
+
+#include <cmath>
+#include <complex>
+#include <vector>
+
+#define MATHPI 4 * atan(1)
+
+//structures for the different coordinate systems. Surface coordinates are for the unit sphere. Polar coordinates are actually Spherical Coordinates.
+//all coordinates are complex.
+struct SurfaceCoord
+{
+public:
+
+    //zenith angle.
+    std::complex<double> phi = 0;
+    //azimuthal angle.
+    std::complex<double> theta = 0;
+
+    //addition for spherical coordinates.(note, addition here is essentially rotation).
+    SurfaceCoord operator +(SurfaceCoord s2)
+    {
+        SurfaceCoord temp;
+        temp.phi = phi + s2.phi;
+        temp.theta = theta + s2.theta;
+
+        return temp;
+    }
+
+    SurfaceCoord()
+    {
+        theta = 0;
+        phi = 0;
+    }
+
+    SurfaceCoord(std::complex<double> a, std::complex<double> b)
+    {
+        phi = a;
+        theta = b;
+    }
+};
+struct PolarCoord
+{
+    std::complex<double> rho;
+    SurfaceCoord s;
+
+    PolarCoord()
+    {
+        rho = 1;
+    }
+    PolarCoord(std::complex<double> r, SurfaceCoord s1)
+    {
+        rho = r;
+        s = s1;
+    }
+    PolarCoord(SurfaceCoord s1)
+    {
+        rho = 1;
+        s = s1;
+    }
+
+
+
+
+
+
+
+};
+struct RectCoord
+{
+    std::complex<double> x;
+    std::complex<double> y;
+    std::complex<double> z;
+
+    RectCoord() : x(0), y(0), z(0) {}
+    RectCoord(std::complex<double>x0, std::complex<double> y0, std::complex<double> z0) :x(x0), y(y0), z(z0) {}
+    RectCoord(PolarCoord r)
+    {
+        x = r.rho * sin(r.s.theta) * cos(r.s.phi);
+        y = r.rho * sin(r.s.theta) * sin(r.s.phi);
+        z = r.rho * cos(r.s.theta);
+    }
+};
+
+
+//addition and subtraction operators for rectangular coordinates.
+RectCoord operator +(RectCoord a, RectCoord b)
+{
+    return RectCoord(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+RectCoord operator -(RectCoord a, RectCoord b)
+{
+    return RectCoord(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+//dot product for rectangular coordinates.
+std::complex<double> dot(RectCoord a, RectCoord b)
+{
+    return a.x * conj(b.x) + a.y * conj(b.y) + a.z * conj(b.z);
+}
+
+//vector cross product.
+RectCoord cross(RectCoord a, RectCoord b)
+{
+    RectCoord temp;
+    temp.x = a.y * b.z - a.z * b.y;
+    temp.y = a.z * b.x - a.x * b.z;
+    temp.z = a.x * b.y - a.y * b.x;
+
+    return temp;
+}
+
+//Spherical and rectangular conversion functions.
+PolarCoord RectToSphere(RectCoord x)
+{
+    PolarCoord temp;
+    temp.rho = sqrt(x.x * x.x + x.y * x.y + x.z * x.z);
+    if (norm(x.x) > 10e-9)
+        temp.s.phi = atan(x.y / x.x);
+    else
+        temp.s.phi = MATHPI / 2;
+
+    if (norm(temp.rho) > 10e-9)
+        temp.s.theta = acos(x.z / temp.rho);
+    else
+        temp.s.theta = 0;
+
+    return temp;
+
+}
+RectCoord SphereToRect(PolarCoord r)
+{
+    return RectCoord(r);
+}
+
+//Polar coordinate arithmetic operators.
+PolarCoord operator +(PolarCoord r, PolarCoord q)
+{
+    return RectToSphere(SphereToRect(r) + SphereToRect(q));
+}
+PolarCoord operator -(PolarCoord r, PolarCoord q)
+{
+    return RectToSphere(SphereToRect(r) - SphereToRect(q));
+}
+PolarCoord operator *(std::complex<double> a, PolarCoord x)
+{
+    return PolarCoord(a * x.rho, SurfaceCoord(x.s));
+}
+
+//Polar Coordinate dot product operator.
+std::complex<double> dot(PolarCoord x, PolarCoord y)
+{
+    return dot(SphereToRect(x), SphereToRect(y));
+}
+
+//returns the norm squared of the given polar coordinates.
+double norm(PolarCoord x)
+{
+    return norm(x.rho);
+}
+
+//Encapulation of a Surface to scalar function. In all such encapsulations, to create a function, create a derived class and implement the function operator().
+//Then create a variable of the derived type and "call it". See code in main file for examples. In particular the rho function(immediately before main function)
+// is the most simple.
+class SurfaceScalarFunction
+{
+public:
+    virtual std::complex<double> operator()(SurfaceCoord s) = 0;
+
+};
+
+//Computes the numerical gradient of a Surface to Scalar function using a symmetric difference with specified stepsize.
+//kappa exists to prevent division by zero and should be set small for accuracy.
+SurfaceCoord surfaceGrad(SurfaceScalarFunction* u, SurfaceCoord s, double stepsize, double kappa)
+{
+    SurfaceCoord temp;
+    SurfaceCoord thetaPlus = SurfaceCoord(s.phi, s.theta + stepsize / 2);
+    SurfaceCoord thetaMinus = SurfaceCoord(s.phi, s.theta - stepsize / 2);
+
+    temp.theta = (*u)(thetaPlus).real() - (*u)(thetaMinus).real();
+    temp.theta /= stepsize;
+
+    SurfaceCoord phiPlus = SurfaceCoord(s.phi + stepsize / 2, s.theta);
+    SurfaceCoord phiMinus = SurfaceCoord(s.phi - stepsize / 2, s.theta);
+
+    temp.phi = (*u)(phiPlus).real() - (*u)(phiMinus).real();
+    temp.phi /= sin(s.theta) * stepsize + (1 + kappa) * stepsize;
+
+    return temp;
+}
+
+//Encapsulates a Spherical coordinate to Scalar function.
+class SphericalScalarFunction
+{
+public:
+    virtual std::complex<double> operator()(PolarCoord) = 0;
+};
+
+//Encapsulates a Spherical coordinate to Spherical Coordinate function.
+class SphericalVectorField
+{
+public:
+    virtual PolarCoord operator()(PolarCoord) = 0;
+};
+
+//Serves as a container allowing a Sum of vector fields to be called as a single function. to add a new function to the sum,
+// call the append function and pass an encapsulated function to it.
+class VectorFieldSum : public SphericalVectorField
+{
+private:
+
+    int numterms;
+    std::vector<SphericalVectorField* > fs;
+public:
+
+    VectorFieldSum() { numterms = 0; }
+    VectorFieldSum(SphericalVectorField& f, SphericalVectorField& g)
+    {
+        numterms = 2;
+        fs.push_back(&f);
+        fs.push_back(&g);
+    }
+
+    VectorFieldSum(VectorFieldSum f, SphericalVectorField& g)
+    {
+        numterms = f.numterms + 1;
+        fs = f.fs;
+        fs.push_back(&g);
+    }
+
+    VectorFieldSum(SphericalVectorField& f, VectorFieldSum g)
+    {
+        numterms = g.numterms + 1;
+        fs.resize(numterms);
+        fs.push_back(&f);
+        for (int i = 0; i < g.numterms; i++)
+            fs[i + 1] = g.fs[i];
+
+    }
+
+    VectorFieldSum(VectorFieldSum f, VectorFieldSum g)
+    {
+        numterms = g.numterms + f.numterms;
+
+        fs = f.fs;
+        fs.resize(numterms);
+
+        for (int i = 0; i < g.numterms; i++)
+            fs[i + f.numterms] = g.fs[i];
+
+
+    }
+
+    void append(SphericalVectorField& f)
+    {
+        fs.push_back(&f);
+        numterms++;
+    }
+
+
+    PolarCoord operator()(PolarCoord x)
+    {
+        PolarCoord temp = (0.0, SurfaceCoord(0.0, 0.0));
+        for (int i = 0; i < numterms; i++)
+        {
+            temp = temp + (*(fs[i]))(x);
+        }
+
+        return temp;
+    }
+
+};
+
+
+//Computes the L2 inner product of two vector fields on the Surface of a unit sphere using a trapezoidal rule in both coordinates.
+std::complex<double> L2InnerProduct(SphericalVectorField* f1, SphericalVectorField* f2, int n)
+{
+    std::complex<double> total = 0;
+
+    for (double p = 0.0; p < MATHPI; p += MATHPI / n)
+        for (double t = 0.0; t < 2.0 * MATHPI; t += 2.0 * MATHPI / n)
+        {
+            SurfaceCoord s(p, t);
+            PolarCoord x(s);
+            total += sin(p) * dot((*f1)(x), (*f2)(x)) * 2.0 * (MATHPI / n) * (MATHPI / n);
+        }
+
+    return total;
+}
+
+//computes the L2 difference of two vector fields on the Surface of a unit sphere using a trapezoidal rule in both coordinates.
+std::complex<double> L2Difference(SphericalVectorField* f1, SphericalVectorField* f2, int n)
+{
+    std::complex<double> total = 0;
+
+    for (double p = 0.0; p < MATHPI; p += MATHPI / n)
+        for (double t = 0.0; t < 2.0 * MATHPI; t += 2.0 * MATHPI / n)
+        {
+            SurfaceCoord s(p, t);
+            PolarCoord x(s);
+            RectCoord diff = RectCoord((*f1)(x)) - RectCoord((*f2)(x));
+            total += sin(p) * dot(diff, diff) * 2.0 * (MATHPI / n) * (MATHPI / n);
+        }
+
+    return total;
+}
+
+//computes the sup norm of two vector fields on the surface of a unit sphere.
+double LInfDifference(SphericalVectorField* f1, SphericalVectorField* f2, int n)
+{
+    double sup = 0;
+    double curr = 0;
+
+    for (double p = 0.0; p < MATHPI; p += MATHPI / n)
+        for (double t = 0.0; t < 2.0 * MATHPI; t += 2.0 * MATHPI / n)
+        {
+            SurfaceCoord s(p, t);
+            PolarCoord x(s);
+            RectCoord diff = RectCoord((*f1)(x)) - RectCoord((*f2)(x));
+            curr = sqrt(dot(diff , diff)).real();
+            if (curr > sup)
+                sup = curr;
+
+        }
+
+    return sup;
+}
